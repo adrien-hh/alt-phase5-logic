602fdb84c21564a29fab81bb2ebcb5311ce9f001
Adrien Huhn <adrien.huhn@gmail.com>
2026-03-13 14:22:21 +0100

    feat: main method running all methods for output examples

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.alt.exercises.ArrayObjectExercises;
+import org.alt.exercises.ObjectExercises;
+import org.alt.exercises.StringExercises;
+
+public class Main {
+    public static void main(String[] args) {
+        StringExercises.run();
+        ObjectExercises.run();
+        ArrayObjectExercises.run();
+    }
+}
